7e525493f624edb98b9ab839e8032ca5c807143e
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2022-10-24 19:52:12 +0300

    add canvas

--- src/sample/GameOfLifeController.java
+++ src/sample/GameOfLifeController.java
@@ -2,8 +2,9 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
 
 public class GameOfLifeController {
 
@@ -11,7 +12,14 @@ public class GameOfLifeController {
     private Button btn;
 
     @FXML
-    private GridPane grid;
+    private Canvas cnv;
+
+    private GraphicsContext gc;
+
+    public void initialize()
+    {
+        gc = cnv.getGraphicsContext2D();
+    }
 
     @FXML
     void nextGeneration(ActionEvent event) {
