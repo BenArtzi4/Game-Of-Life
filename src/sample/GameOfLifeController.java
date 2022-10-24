4c26b0c073e47ca032230b71f3c7ede972f6b157
Ben Artzi Shachar <gal.ben.artzi.shachar@intel.com>
2022-10-24 19:26:14 +0300

    Add sceneBuilder file and controller

---
+++ src/sample/GameOfLifeController.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class GameOfLifeController {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    void nextGeneration(ActionEvent event) {
+
+    }
+
+}
